42dc4d932f7c61a323ec527e69198c98b435f51e
Iliyaz Syed <iliyaz.syed1995@gmail.com>
2019-03-20 01:08:03 +0530

    2nd

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="container">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#!">React Testing</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
